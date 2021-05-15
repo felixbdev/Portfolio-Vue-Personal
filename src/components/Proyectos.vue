93bc8d9770a0c8a1f90ff5f7eed064e5cfcde5c1
felixb <felix.bonillo3@gmail.com>
2021-05-15 10:18:37 -0400

    Generado de botones, pero falta borrar duplicados

--- src/components/Proyectos.vue
+++ src/components/Proyectos.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="row justify-content-center">
+    <div class="button" v-for="button in button" :key="button.id">
+      <button>{{button.language}}</button>
+    </div>
+
     <div class="card mt-3 col-12 col-lg-3 justify-content-center " id="card-api" v-for="item in proyecto" :key="item.id">
       <h5>{{item.name}}</h5>
       <span>{{item.id}}</span>
+      <span>{{item.language}}</span>
     </div>
   </div>
 
@@ -13,7 +18,8 @@ export default {
   name: "Proyectos",
   data(){
     return {
-      proyecto: {}
+      proyecto: {},
+      button:[],
     }
   },
   methods: {
@@ -21,15 +27,22 @@ export default {
       try {
         const data = await fetch ('https://api.github.com/users/felixbdev/repos')
         const object = await data.json()
-        console.log(object)
+        //console.log(object)
         this.proyecto = object
+        this.button = object
+        console.log(this.button)
+
       } catch (error) {
         console.log(error)
       }
+    },
+    filtrarArray(){
+      console.log(this.button)
     }
   },
   created(){
     this.consumirApi()
+    this.filtrarArray()
   }
 }
 </script>
