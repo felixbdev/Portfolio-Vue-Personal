352b9bb8013b07a1eff32dc10945aad0579a6475
felixb <felix.bonillo3@gmail.com>
2021-05-16 13:50:58 -0400

    Anadido las experiencias

---
+++ src/components/Experiencias.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <h3>{{title}}</h3>
+    <span>{{desde}}-{{hasta}}</span>
+
+    <h5>{{experiencia}}</h5>
+    <p>{{resume}}</p>
+    <hr>
+
+</template>
+
+<script>
+export default {
+  name: "Experiencias",
+  props:{
+    title: String,
+    desde: Number,
+    hasta: Number,
+    experiencia: String,
+    resumen: String
+  }
+
+}
+</script>
+
+<style>
+
+</style>
