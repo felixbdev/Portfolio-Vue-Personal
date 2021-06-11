62e259b12edd13d5c66c5ebcd77a1ccebd8975cd
felixb <felix.bonillo3@gmail.com>
2021-06-10 20:57:54 -0400

    Resuelto transition

--- src/App.vue
+++ src/App.vue
@@ -70,4 +70,15 @@ export default {
   opacity: 1;
   transform: translate(0, 0);
 }
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,5 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import "@fortawesome/fontawesome-free/css/fontawesome.min.css"
 
-
-
 createApp(App).use(router).mount('#app')
 
